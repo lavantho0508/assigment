ee11e76cb63b84cf7a681ff1af764e87a207c0e4
lavantho0508 <91973892+lavantho0508@users.noreply.github.com>
2022-02-22 17:31:35 +0700

    thohp

---
+++ admin/login-admin.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">      
+        <title></title>
+        <link rel="stylesheet" href="../css/bootstrap.min.css"/>
+    </head>
+    <body>
+        <form action="" method="POST" class="form-control">
+            <label for="username" class="form-label">Username</label>
+            <input type="text" name="username" class="form-control" required="" id="username">
+             <label for="passwd" class="form-label">Password</label>
+             <input type="text" name="passwd" class="form-control" required="" id="passwd"><!-- comment -->   
+             <input type="submit" name="sumit" class="btn btn-primary">
+        </form>
+        <?php
+       
+        ?>
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/popper.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    </body>
+</html>
